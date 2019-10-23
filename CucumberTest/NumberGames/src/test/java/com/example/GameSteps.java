cfd4e194be45fe8b5a67db67d4db3848c5490b6c
cuiguoxin1 <18524355221@163.com>
2019-10-23 15:27:52 +0800

    cucumber test

---
+++ CucumberTest/NumberGames/src/test/java/com/example/GameSteps.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameSteps {
+    private Game _target;
+    private String _actualResult;
+
+    @Given("^I am officiating a \"([^\"]*)\" game$")
+    public void I_am_officiating_a_game(String gameTypeName) {
+        GameType gameType = GameType.valueOf(gameTypeName);
+        _target = GameFactory.createGame(gameType);
+    }
+
+    @When("^the number (\\d+) is played$")
+    public void the_number_is_played(int playedNumber) {
+        _actualResult = _target.checkPlay(playedNumber);
+    }
+
+    @Then("^I should be told the correct answer is \"([^\"]*)\"$")
+    public void I_should_be_told_the_correct_answer_is(String expectedResult) {
+        assertEquals(expectedResult, _actualResult);
+    }
+
+    int a;
+    int b;
+    @Given("^I have a number num is 1$")
+    public void aaa() {
+        a = 1;
+    }
+
+    @When("^b = 1$")
+    public void sss() {
+        b = 1;
+    }
+
+    @Then("^a = b$")
+    public void ccc() {
+        assertEquals(a, b);
+    }
+}
